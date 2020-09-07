3a1927fa3194c5caae36a63ba6f53a4db70204ee
unknown <mkadmin@MKLAB.INT>
2020-09-06 23:49:24 -0400

    updated script to create accounts in AD

--- MKLAB/Create-ADUsersinBulk.ps1
+++ MKLAB/Create-ADUsersinBulk.ps1
@@ -1,12 +1,34 @@
-List$ = 1..5 | Foreach-object {
+$OU = "OU=TestUserAccounts,DC=MKLAB,DC=INT"
+$password = ConvertTo-SecureString "Passw0rd123#" -AsPlainText -Force
+
+$i = 1
+
+1..1000 | Foreach-object {
     $First = invoke-generate "[person both first]"
+    $Middle = invoke-generate "[person both first]"
     $Last = invoke-generate "[person both last]"
-    $SAM = -join (($First.Substring(0,1)),($Last))
+    $SAM = -join (($First.Substring(0,1)),($Middle.Substring(0,1)),($Last))
 
     $nameinfo = [PSCustomObject]@{
-        First = $first
+        First = $First
         Last = $Last
         SAMAcct = $SAM
+        DisplayName = $First, $Middle, $Last -join " "
     }
-    $nameinfo
+
+    $NewUserParams = @{
+    Name = $nameinfo.SamAcct
+    DisplayName = $nameinfo.DisplayName
+    GivenName = $nameinfo.First
+    Surname = $nameinfo.Last
+    Path = $OU
+    AccountPassword = $password
+    Enabled = $true
+    Server = "DC01"
+    }
+    
+    $i
+    $i++
+    New-ADUser @NewUserParams 
+    
 }
